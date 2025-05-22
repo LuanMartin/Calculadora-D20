6f791d6176e967447d4d43c28f9539c2c5af30c4
LuanMartin <luanmartin21@gmail.com>
2025-05-21 21:07:09 -0300

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,26 @@
+let forbase = document.getElementById("forbase")
+let conbase = document.getElementById("conbase")
+let intbase = document.getElementById("intbase")
+let sabBase = document.getElementById("sabBase")
+let carbase = document.getElementById("carbase")
+
+
+let forD20 = document.getElementById("forD20")
+let conD20 = document.getElementById("conD20")
+let intD20 = document.getElementById("intD20")
+let sabD20 = document.getElementById("sabD20")
+let carD20 = document.getElementById("carD20")
+
+let forMod = document.getElementById("forMod")
+let conMod = document.getElementById("conMod")
+let intMod = document.getElementById("intMod")
+let sabMod = document.getElementById("sabMod")
+let carMod = document.getElementById("carMod")
+
+let forTotal = document.getElementById("forTotal")
+let conTotal = document.getElementById("conTotal")
+let intTotal = document.getElementById("intTotal")
+let sabTotal = document.getElementById("sabTotal")
+let carTotal = document.getElementById("carTotal")
+
+
